9c35e4cc33cdf8f75e3973e3e03b7ec98046e4bd
tracy <200548417@student.georgianc.on.ca>
2024-03-31 12:45:56 -0400

    add initialize

--- src/main/java/com/example/assignment2/searchSceneController.java
+++ src/main/java/com/example/assignment2/searchSceneController.java
@@ -21,7 +21,29 @@ public class searchSceneController {
     @FXML
     private TextField searchTextField;
 
+    @FXML
+    private Button getDetailButton;
+
     public static ArrayList<Cat> catLists;
+
+    public static List<String> catUniqueOrigin;
+
+    @FXML
+    private void initialize(){
+
+        listView.getItems().clear();
+        listView.getItems().add("Search Instruction:");
+        listView.getItems().add("Please type one of the countries below in search bar.");
+        listView.getItems().add("And press search button to check the result.");
+
+        SimpleApiHttpClient test = new SimpleApiHttpClient();
+
+        catLists = test.parseJsonPosts(test.fetchData("https://api.thecatapi.com/v1/breeds"));
+        catUniqueOrigin = catLists.stream().map(Cat::getOrigin).distinct().collect(Collectors.toList());
+        listView.getItems().addAll(catUniqueOrigin);
+
+        getDetailButton.setVisible(false);
+    }
 
     @FXML
     private ArrayList<Cat> searchForCats(ActionEvent event) throws IOException, InterruptedException {
@@ -33,8 +55,19 @@ public class searchSceneController {
 
         List<String> catNames = catLists.stream().filter(cat -> cat.getOrigin().equalsIgnoreCase(origin)).map(Cat::getName).collect(Collectors.toList());
 
-        listView.getItems().clear();
-        listView.getItems().addAll(catNames);
+        if(catNames.size() > 0){
+            listView.getItems().clear();
+            listView.getItems().addAll(catNames);
+
+            getDetailButton.setVisible(true);
+        }else{
+            listView.getItems().clear();
+            listView.getItems().add("Sorry, no cats' origin from this country :(");
+            listView.getItems().add("Try those countries as listed below.");
+            listView.getItems().addAll(catUniqueOrigin);
+
+            getDetailButton.setVisible(false);
+        }
 
         return catLists;
     }
